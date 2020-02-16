723bd541dba846eef0e0f6f63eed586a4b09b939
Paulmburu <paulmburu53@gmail.com>
2020-02-16 03:09:00 +0300

    Movies api service, consume apis

--- app/src/main/java/tk/paulmburu/moviesreview/network/MoviesApiService.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/network/MoviesApiService.kt
@@ -4,10 +4,10 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import tk.paulmburu.moviesreview.models.MoviesProperties
 
 private const val BASE_URL = "https://api.themoviedb.org/3/"
 
