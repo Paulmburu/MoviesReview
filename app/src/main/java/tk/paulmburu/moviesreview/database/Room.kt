ed8b0b6ff6c8f10a85a7c52bfb134e5ae6e6eb9d
Paulmburu <paulmburu53@gmail.com>
2020-04-03 22:21:05 +0300

    delete room query

--- app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
@@ -14,6 +14,10 @@ interface MovieDao {
     // SQL @Insert insertAll() that replaces on conflict (or upsert).
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseMovie)
+
+    // SQL @Query deleteMovies() function that deletes all movies
+    @Query("DELETE FROM databasemovie")
+    fun deleteAllMovies()
 }
 
 // An abstract MoviesDatabase class that extends RoomDatabase.
