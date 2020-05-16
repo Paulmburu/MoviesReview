912df02fe29a69da2e00c4a19cbfeefb064ee3c0
Paulmburu <paulmburu53@gmail.com>
2020-05-16 21:20:19 +0300

    fetch non-LiveData data from db

--- app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
@@ -9,7 +9,7 @@ interface MovieDao {
 
     // SQL @Query getMovies() function that returns a List of DatabaseMovieResult livedata.
     @Query("select * from databasemovie")
-    fun getMovies(): LiveData<List<DatabaseMovie>>
+    fun getMovies(): List<DatabaseMovie>
 
     // SQL @Insert insertAll() that replaces on conflict (or upsert).
     @Insert(onConflict = OnConflictStrategy.REPLACE)
