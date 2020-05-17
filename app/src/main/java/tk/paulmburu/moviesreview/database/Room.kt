da621de76074a0f6cb53433563bf7e1870dbbffb
Paulmburu <paulmburu53@gmail.com>
2020-05-17 23:24:52 +0300

    acess tables data from the MovieDao

--- app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/database/Room.kt
@@ -7,21 +7,33 @@ import androidx.room.*
 @Dao
 interface MovieDao {
 
-    // SQL @Query getMovies() function that returns a List of DatabaseMovieResult livedata.
-    @Query("select * from databasemovie")
-    fun getMovies(): List<DatabaseMovie>
+    // SQL @Query getMovies() function that returns a List of DatabaseMovieResult
+    @Query("select * from popularmovies")
+    fun getPopularMovies(): List<PopularMovies>
 
     // SQL @Insert insertAll() that replaces on conflict (or upsert).
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(vararg videos: DatabaseMovie)
+    fun insertAllPopularMovies(vararg videos: PopularMovies)
 
     // SQL @Query deleteMovies() function that deletes all movies
-    @Query("DELETE FROM databasemovie")
-    fun deleteAllMovies()
+    @Query("DELETE FROM popularmovies")
+    fun deleteAllPopularMovies()
+
+    // SQL @Query getUpcomingMovies() function that returns a List of DatabaseMovieResult
+    @Query("select * from upcomingmovies")
+    fun getUpcomingMovies(): List<UpcomingMovies>
+
+    // SQL @Insert insertAllUpcomingMovies() that replaces on conflict (or upsert).
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllUpcomingMovies(vararg videos: UpcomingMovies)
+
+    // SQL @Query deleteUpcomingMovies() function that deletes all movies
+    @Query("DELETE FROM upcomingmovies")
+    fun deleteAllUpcomingMovies()
 }
 
 // An abstract MoviesDatabase class that extends RoomDatabase.
-@Database(entities = [DatabaseMovie::class], version = 1)
+@Database(entities = [PopularMovies::class, UpcomingMovies::class], version = 1)
 abstract class MoviesDatabase : RoomDatabase() {
     abstract val movieDao: MovieDao
 }
