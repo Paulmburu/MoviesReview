f359e19c4da0b134feb820d6ef3674604505ffb3
Paulmburu <paulmburu53@gmail.com>
2020-05-18 23:35:24 +0300

    setup toolbar with navcontroller

--- app/src/main/java/tk/paulmburu/moviesreview/MainActivity.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/MainActivity.kt
@@ -2,10 +2,16 @@ package tk.paulmburu.moviesreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -19,55 +25,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var moviesRepository: MoviesRepository
 
-    //    Create a coroutine Job and a CoroutineScope using the Main Dispatcher
-    private var viewModelJob = Job()
-    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val database = getDatabase(this.application)
-        moviesRepository = MoviesRepository(database)
-
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph )
+
+        findViewById<Toolbar>(R.id.my_toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.overflow_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        return when (item?.itemId) {
-            R.id.id_popular_movies_menu -> {
-                Toast.makeText(this,"Popular Movies", Toast.LENGTH_SHORT).show()
-                coroutineScope.launch {
-                    moviesRepository.refreshMovies()
-                }
-                true
-            }
-
-            R.id.id_upcoming_movies_menu-> {
-                Toast.makeText(this,"Upcoming Movies", Toast.LENGTH_SHORT).show()
-//                moviesRepository.getUpcomingMovies()
-                coroutineScope.launch {
-                    moviesRepository.getUpcomingMovies()
-                }
-
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        viewModelJob.cancel()
-    }
 }
