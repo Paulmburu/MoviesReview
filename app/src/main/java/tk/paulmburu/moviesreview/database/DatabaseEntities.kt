f4c240337bee19a25715c4cc7887cf6a4d478ccf
Paulmburu <paulmburu53@gmail.com>
2020-05-18 23:27:02 +0300

    create Entities PopularMovies & UpcomingMovies

--- app/src/main/java/tk/paulmburu/moviesreview/database/DatabaseEntities.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/database/DatabaseEntities.kt
@@ -4,30 +4,39 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import tk.paulmburu.moviesreview.domain.Movie
 
+sealed class DatabaseEntities(){
+    abstract val title: String
+    abstract val overview: String
+    abstract val release_date: String
+    abstract val vote_average: String
+    abstract val poster_path: String
+    abstract val original_language: String
+}
+
 @Entity
 data class PopularMovies constructor(
     @PrimaryKey
-    open val title: String,
-    open val overview: String,
-    open val release_date: String,
-    open val vote_average: String,
-    open val poster_path: String,
-    open val original_language: String
-)
+    override val title: String,
+    override val overview: String,
+    override val release_date: String,
+    override val vote_average: String,
+    override val poster_path: String,
+    override val original_language: String
+) : DatabaseEntities()
 
 @Entity
 data class UpcomingMovies constructor(
     @PrimaryKey
-    val title: String,
-    val overview: String,
-    val release_date: String,
-    val vote_average: String,
-    val poster_path: String,
-    val original_language: String
-)
+    override val title: String,
+    override val overview: String,
+    override val release_date: String,
+    override val vote_average: String,
+    override val poster_path: String,
+    override val original_language: String
+) : DatabaseEntities()
 
 
-fun List<PopularMovies>.asDomainModel(): List<Movie> {
+fun List<DatabaseEntities>.asDomainModel(): List<Movie> {
     return map {
         Movie(
             title = it.title,
