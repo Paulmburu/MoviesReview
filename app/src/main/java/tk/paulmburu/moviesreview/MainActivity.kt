c23f7118cd5e16160930d40f121f73a264f7a7b4
Paulmburu <paulmburu53@gmail.com>
2020-05-19 15:55:06 +0300

    check for in app updates when launching app

--- app/src/main/java/tk/paulmburu/moviesreview/MainActivity.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/MainActivity.kt
@@ -2,28 +2,22 @@ package tk.paulmburu.moviesreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import tk.paulmburu.moviesreview.database.getDatabase
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.UpdateAvailability
 import tk.paulmburu.moviesreview.databinding.ActivityMainBinding
-import tk.paulmburu.moviesreview.repository.MoviesRepository
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var moviesRepository: MoviesRepository
+    lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +31,45 @@ class MainActivity : AppCompatActivity() {
         findViewById<Toolbar>(R.id.my_toolbar)
             .setupWithNavController(navController, appBarConfiguration)
 
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        subscribeImmediateInAppUpdateObserver()
+    }
+
+    fun subscribeImmediateInAppUpdateObserver(){
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    12)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                if (appUpdateInfo.updateAvailability()
+                    == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+                ) {
+                    // If an in-app update is already running, resume the update.
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        IMMEDIATE,
+                        this,
+                        12
+                    );
+                }
+            }
     }
 
 }
