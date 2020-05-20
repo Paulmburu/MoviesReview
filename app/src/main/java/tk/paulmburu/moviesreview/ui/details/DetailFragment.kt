3d24d7851cb26a0e936d1421c5a6a1326b06a73d
Paulmburu <paulmburu53@gmail.com>
2020-05-20 22:45:45 +0300

    fixed binding viewmodel

--- app/src/main/java/tk/paulmburu/moviesreview/ui/details/DetailFragment.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/ui/details/DetailFragment.kt
@@ -2,6 +2,7 @@ package tk.paulmburu.moviesreview.ui.details
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -36,6 +37,7 @@ class DetailFragment : DaggerFragment(){
         viewModel = ViewModelProviders.of(
             this, providerFactory).get(DetailViewModel::class.java)
         viewModel.setSelectedMovie(movie)
+        binding.viewModel = viewModel
         return binding.root
 
     }
