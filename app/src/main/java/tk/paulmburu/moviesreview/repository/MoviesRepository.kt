2fbb630a8553ef8f996228079bf6d588b6f660bf
Paulmburu <paulmburu53@gmail.com>
2020-05-20 19:21:56 +0300

    inject MoviesApi and MoviesDatabase

--- app/src/main/java/tk/paulmburu/moviesreview/repository/MoviesRepository.kt
+++ app/src/main/java/tk/paulmburu/moviesreview/repository/MoviesRepository.kt
@@ -4,22 +4,25 @@ package tk.paulmburu.moviesreview.repository
 import tk.paulmburu.moviesreview.database.MoviesDatabase
 import tk.paulmburu.moviesreview.database.asDomainMovieModel
 import tk.paulmburu.moviesreview.domain.Movie
-import tk.paulmburu.moviesreview.network.*
+import tk.paulmburu.moviesreview.network.MoviesApiService
+import tk.paulmburu.moviesreview.network.asDatabasePopularMoviesModel
+import tk.paulmburu.moviesreview.network.asDatabaseUpcomingMoviesModel
 import tk.paulmburu.moviesreview.utils.ResultState
 import tk.paulmburu.moviesreview.utils.Success
 import tk.paulmburu.moviesreview.utils.executeNonBlocking
+import javax.inject.Inject
 
-class MoviesRepository (private val database: MoviesDatabase){
+class MoviesRepository @Inject constructor(private val moviesApiService: MoviesApiService ,private val database: MoviesDatabase){
 
 
     suspend fun getAvailablePopularMovies(): ResultState<List<Movie>>{
         return executeNonBlocking {
             if(database.movieDao.getPopularMovies().isNullOrEmpty()){
-                val response = MoviesApi.retrofitService.getPopularMovies()
+                val response = moviesApiService.getPopularMovies()
                 if(response.results.size == 0 )
                     Success(emptyList<Movie>())
                 else{
-                    val result = MoviesApi.retrofitService.getPopularMovies()
+                    val result = moviesApiService.getPopularMovies()
                     database.movieDao.insertAllPopularMovies(*result.asDatabasePopularMoviesModel())
                     Success(database.movieDao.getPopularMovies().asDomainMovieModel())
                 }
@@ -30,11 +33,11 @@ class MoviesRepository (private val database: MoviesDatabase){
     suspend fun getAvailableUpcomingMovies(): ResultState<List<Movie>>{
         return executeNonBlocking {
             if(database.movieDao.getUpcomingMovies().isNullOrEmpty()){
-                val response = MoviesApi.retrofitService.getUpcomingMovies()
+                val response = moviesApiService.getUpcomingMovies()
                 if(response.results.size == 0 )
                     Success(emptyList<Movie>())
                 else{
-                    val result = MoviesApi.retrofitService.getUpcomingMovies()
+                    val result = moviesApiService.getUpcomingMovies()
                     database.movieDao.insertAllUpcomingMovies(*result.asDatabaseUpcomingMoviesModel())
                     Success(database.movieDao.getUpcomingMovies().asDomainMovieModel())
                 }
